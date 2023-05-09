21404c40c2a326ae13a1b08272dca7a1e6fbb58d
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2023-05-09 14:08:55 -0300

    shows possible moves

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,82 +2,15 @@ import { V2_MetaFunction, json } from "@remix-run/node";
 import { PieceType, Piece, Direction, TableResponse, PossibleMove } from "./resources/types";
 import { userMove, currentState, possibleMoves } from "./resources/services/tableService";
 import { useLoaderData } from "@remix-run/react";
-import { BLACK_TILE, PLAYER_1_PIECE_COLOR, PLAYER_2_PIECE_COLOR, POSSIBLE_MOVE_COLOR, WHITE_TILE } from "./resources/colors";
+import { BLACK_TILE_COLOR, PIECE_FROM_COLOR, PLAYER_1_PIECE_COLOR, PLAYER_2_PIECE_COLOR, POSSIBLE_MOVE_COLOR, WHITE_TILE_COLOR } from "./resources/colors";
+import { useEffect, useState } from "react";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
-let possibleMovesObj: PossibleMove[] = [{"captures":0,"movesLog":[{"from":[5,3],"captured":null,"to":[4,4],"playerTurn":"BLUE","moveType":"MOVE","direction":"FORWARD_LEFT","description":"Blue in [5,3] moved forward left to [4,4]"}]},{"captures":0,"movesLog":[{"from":[5,3],"captured":null,"to":[4,2],"playerTurn":"BLUE","moveType":"MOVE","direction":"FORWARD_RIGHT","description":"Blue in [5,3] moved forward right to [4,2]"}]}];
-
-const boardBgColor = (line:number, column:number) => {
-  return (line + column) % 2 == 0 ? WHITE_TILE : BLACK_TILE;
-}
-
-const sessionId: string = '123';
-
-const tableStyle = {
-  margin: "0",
-  padding: "0",
-  border: "0.3em solid black"
-}
-
-const lineStyle = (line:number, column:number) => ({
-  width: "6em",
-  height: "6em",
-  margin: "0",
-  padding: "0",
-  backgroundColor: boardBgColor(line, column),
-  border: "0.15em solid black"
-})
-
-const pieceWrapperStyle = (line: number, column: number) => {
-  
-  let found = possibleMovesObj.find(pm => pm.movesLog.find(
-    ml => (ml.from[0] === line && ml.from[1] === column) ||
-    (ml.to[0] === line && ml.to[1] === column)))
-
-  let color;
-  
-  if(found) {
-    color = POSSIBLE_MOVE_COLOR; 
-  }
-
-  return ({
-    width: "100%",
-    height: "100%",
-    backgroundColor: color,
-    display: "flex",
-    alignItems:"center",
-    justifyContent:"center"
-  })
-}
-
-const pieceStyle = (piece: Piece) => {
-
-  const color = piece.type == PieceType.RED ? PLAYER_1_PIECE_COLOR : PLAYER_2_PIECE_COLOR
-  return {
-    cursor: "pointer",
-    margin: "0",
-    borderRadius: "4.5em",
-    backgroundColor: color,
-    width: "4.5em",
-    height: "4.5em"
-  }
-}
-
-const loadPossibleMoves = async (line: number, column: number) => {
-  let data = await possibleMoves({
-      sessionId,
-      line,
-      column
-    })
-
-  Object.assign(possibleMovesObj, data);
-}
-
 export async function loader() {
-
+  
   let data = await currentState({
     sessionId: "123"
   })
@@ -92,30 +25,119 @@ export async function loader() {
   return data
 }
 
+export default function Index() {
 
-const Piece = ({tablePieces, line, column}: {tablePieces: TableResponse, line: number, column: number}) => {
-
-  let pieceFinder = (list: Piece[], line: number, column: number): Piece | undefined => {
-    return list.find(piece => piece.line == line && piece.column == column)
+  let tableState: {tableResponse: TableResponse, possibleMoves: PossibleMove[]} = {
+    tableResponse: useLoaderData(),
+    possibleMoves: []
   }
 
-  let movesCore = tablePieces.movesCore;
+  const [data, setData] = useState(tableState);
 
-  if(!movesCore) return null;
 
-  let piece =
-    pieceFinder(movesCore.redPieces, line, column) || 
-    pieceFinder(movesCore.bluePieces, line, column);
+  const boardBgColor = (line:number, column:number) => {
+    return (line + column) % 2 == 0 ? WHITE_TILE_COLOR : BLACK_TILE_COLOR;
+  }
+  
+  const sessionId: string = '123';
+  
+  const tableStyle = {
+    margin: "0",
+    padding: "0",
+    border: "0.3em solid black"
+  }
+  
+  const lineStyle = (line:number, column:number) => ({
+    width: "6em",
+    height: "6em",
+    margin: "0",
+    padding: "0",
+    backgroundColor: boardBgColor(line, column),
+    border: "0.15em solid black"
+  })
+  
+  const pieceWrapperStyle = (line: number, column: number) => {
+  
+    let piecesComparator = (pieceCoordinates: number[]) => {
+      return !!pieceCoordinates &&
+        pieceCoordinates[0] === line &&
+        pieceCoordinates[1] === column
+    }
+  
+    let moves = data.possibleMoves.
+      map(pm => pm.movesLog).
+      reduce((x, y) => x.concat(y), []);
+  
+    let moveFound = moves.find(ml => 
+      piecesComparator(ml.from) ||
+      piecesComparator(ml.to) ||
+      piecesComparator(ml.captured))
+  
+    let color;
+    
+    if(moveFound) {
+      if(piecesComparator(moveFound.from)) {
+        color = PIECE_FROM_COLOR;
+      } else if(piecesComparator(moveFound.to)) {
+        color = POSSIBLE_MOVE_COLOR; 
+      } else {
+        color = 'blue'
+      }
+    }
+  
+    return ({
+      width: "100%",
+      height: "100%",
+      backgroundColor: color,
+      display: "flex",
+      alignItems:"center",
+      justifyContent:"center"
+    })
+  }
+  
+  const pieceStyle = (piece: Piece) => {
+  
+    const color = piece.type == PieceType.RED ? PLAYER_1_PIECE_COLOR : PLAYER_2_PIECE_COLOR
+    return {
+      cursor: "pointer",
+      margin: "0",
+      borderRadius: "4.5em",
+      border: "0.1em solid #a0a0a0",
+      backgroundColor: color,
+      width: "4.5em",
+      height: "4.5em"
+    }
+  }
+  
+  const loadPossibleMoves = (line: number, column: number) => {
+    possibleMoves({
+        sessionId,
+        line,
+        column
+      }).then(res => setData({tableResponse: data.tableResponse, possibleMoves: res}))
+  }
 
-  if(!piece) return null;
+  const Piece = ({line, column}: {line: number, column: number}) => {
 
-  return (
-    <div onClick={() => {loadPossibleMoves(line, column)}} style={pieceStyle(piece)}/>
-  )
-}
+    let pieceFinder = (list: Piece[], line: number, column: number): Piece | undefined => {
+      return list.find(piece => piece.line == line && piece.column == column)
+    }
+  
+    let movesCore = data.tableResponse.movesCore;
+  
+    if(!movesCore) return null;
+  
+    let piece =
+      pieceFinder(movesCore.redPieces, line, column) || 
+      pieceFinder(movesCore.bluePieces, line, column);
+  
+    if(!piece) return null;
+  
+    return (
+      <div onClick={() => {loadPossibleMoves(line, column)}} style={pieceStyle(piece)}/>
+    )
+  }
 
-export default function Index() {
-  const data: TableResponse = useLoaderData();
   return (
       <table style={tableStyle}>
         <tbody>
@@ -124,7 +146,7 @@ export default function Index() {
               {Array.from({ length: 8 }, (_value, columnIndex) => (
                 <td key={""+(lineIndex+columnIndex)} style={lineStyle(lineIndex, columnIndex)}>
                   <div style={pieceWrapperStyle(lineIndex, columnIndex)}>
-                    <Piece tablePieces={data} line={lineIndex} column={columnIndex} />
+                    <Piece line={lineIndex} column={columnIndex} />
                   </div>
                 </td>
               ))}
