d3cc48f6e96b5a8c2ce9a1684857d6c94ad5771f
Ícaro Erasmo Souza Barreiro <me@icaroerasmo.com>
2023-05-09 14:34:00 -0300

    refactor

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -2,18 +2,18 @@ import { V2_MetaFunction, json } from "@remix-run/node";
 import { PieceType, Piece, Direction, TableResponse, PossibleMove } from "./resources/types";
 import { userMove, currentState, possibleMoves } from "./resources/services/tableService";
 import { useLoaderData } from "@remix-run/react";
-import { BLACK_TILE_COLOR, PIECE_FROM_COLOR, PLAYER_1_PIECE_COLOR, PLAYER_2_PIECE_COLOR, POSSIBLE_MOVE_COLOR, WHITE_TILE_COLOR } from "./resources/colors";
 import { useEffect, useState } from "react";
+import { lineStyle, pieceStyle, pieceWrapperStyle, tableStyle } from "./resources/styles";
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
+const sessionId = '123'
+
 export async function loader() {
   
-  let data = await currentState({
-    sessionId: "123"
-  })
+  let data = await currentState({sessionId})
 
   let movesCore = data.movesCore;
 
@@ -33,88 +33,9 @@ export default function Index() {
   }
 
   const [data, setData] = useState(tableState);
-
-
-  const boardBgColor = (line:number, column:number) => {
-    return (line + column) % 2 == 0 ? WHITE_TILE_COLOR : BLACK_TILE_COLOR;
-  }
-  
-  const sessionId: string = '123';
-  
-  const tableStyle = {
-    margin: "0",
-    padding: "0",
-    border: "0.3em solid black"
-  }
-  
-  const lineStyle = (line:number, column:number) => ({
-    width: "6em",
-    height: "6em",
-    margin: "0",
-    padding: "0",
-    backgroundColor: boardBgColor(line, column),
-    border: "0.15em solid black"
-  })
-  
-  const pieceWrapperStyle = (line: number, column: number) => {
-  
-    let piecesComparator = (pieceCoordinates: number[]) => {
-      return !!pieceCoordinates &&
-        pieceCoordinates[0] === line &&
-        pieceCoordinates[1] === column
-    }
-  
-    let moves = data.possibleMoves.
-      map(pm => pm.movesLog).
-      reduce((x, y) => x.concat(y), []);
-  
-    let moveFound = moves.find(ml => 
-      piecesComparator(ml.from) ||
-      piecesComparator(ml.to) ||
-      piecesComparator(ml.captured))
-  
-    let color;
-    
-    if(moveFound) {
-      if(piecesComparator(moveFound.from)) {
-        color = PIECE_FROM_COLOR;
-      } else if(piecesComparator(moveFound.to)) {
-        color = POSSIBLE_MOVE_COLOR; 
-      } else {
-        color = 'blue'
-      }
-    }
-  
-    return ({
-      width: "100%",
-      height: "100%",
-      backgroundColor: color,
-      display: "flex",
-      alignItems:"center",
-      justifyContent:"center"
-    })
-  }
-  
-  const pieceStyle = (piece: Piece) => {
-  
-    const color = piece.type == PieceType.RED ? PLAYER_1_PIECE_COLOR : PLAYER_2_PIECE_COLOR
-    return {
-      cursor: "pointer",
-      margin: "0",
-      borderRadius: "4.5em",
-      border: "0.1em solid #a0a0a0",
-      backgroundColor: color,
-      width: "4.5em",
-      height: "4.5em"
-    }
-  }
   
   const loadPossibleMoves = (line: number, column: number) => {
-    possibleMoves({
-        sessionId,
-        line,
-        column
-      }).then(res => setData({tableResponse: data.tableResponse, possibleMoves: res}))
+    possibleMoves({sessionId, line, column}).then(res => setData({tableResponse: data.tableResponse, possibleMoves: res}))
   }
 
   const Piece = ({line, column}: {line: number, column: number}) => {
@@ -145,7 +66,7 @@ export default function Index() {
             <tr className="line">
               {Array.from({ length: 8 }, (_value, columnIndex) => (
                 <td key={""+(lineIndex+columnIndex)} style={lineStyle(lineIndex, columnIndex)}>
-                  <div style={pieceWrapperStyle(lineIndex, columnIndex)}>
+                  <div style={pieceWrapperStyle(data.possibleMoves, lineIndex, columnIndex)}>
                     <Piece line={lineIndex} column={columnIndex} />
                   </div>
                 </td>
---
+++ app/routes/resources/styles.ts
@@ -0,0 +1,76 @@
+import { BLACK_TILE_COLOR, PIECE_FROM_COLOR, PLAYER_1_PIECE_COLOR, PLAYER_2_PIECE_COLOR, POSSIBLE_MOVE_COLOR, WHITE_TILE_COLOR } from "./colors";
+import { Piece, PieceType, PossibleMove } from "./types";
+
+export const boardBgColor = (line:number, column:number) => {
+    return (line + column) % 2 == 0 ? WHITE_TILE_COLOR : BLACK_TILE_COLOR;
+}
+  
+export const tableStyle = {
+    margin: "0",
+    padding: "0",
+    border: "0.3em solid black"
+}
+  
+export const lineStyle = (line:number, column:number) => ({
+    width: "6em",
+    height: "6em",
+    margin: "0",
+    padding: "0",
+    backgroundColor: boardBgColor(line, column),
+    border: "0.15em solid black"
+})
+  
+export const pieceWrapperStyle = (possibleMoves: PossibleMove[], line: number, column: number) => {
+
+    const piecesComparator = (pieceCoordinates: number[]) => {
+        return !!pieceCoordinates &&
+        pieceCoordinates[0] === line &&
+        pieceCoordinates[1] === column
+    }
+
+    const moves = possibleMoves.
+        map(pm => pm.movesLog).
+        reduce((x, y) => x.concat(y), []);
+
+    let moveFound = moves.find(ml => 
+        piecesComparator(ml.from) ||
+        piecesComparator(ml.to) ||
+        piecesComparator(ml.captured))
+
+    let color;
+
+    if(moveFound) {
+        if(piecesComparator(moveFound.from)) {
+        color = PIECE_FROM_COLOR;
+        } else if(piecesComparator(moveFound.to)) {
+        color = POSSIBLE_MOVE_COLOR; 
+        } else {
+        color = 'blue'
+        }
+    }
+
+    return ({
+        width: "100%",
+        height: "100%",
+        backgroundColor: color,
+        display: "flex",
+        alignItems:"center",
+        justifyContent:"center"
+    })
+}
+  
+export const pieceStyle = (piece: Piece) => {
+
+    const color = piece.type == PieceType.RED ?
+        PLAYER_1_PIECE_COLOR : PLAYER_2_PIECE_COLOR
+
+    return {
+        cursor: "pointer",
+        margin: "0",
+        borderRadius: "4.5em",
+        border: "0.1em solid #a0a0a0",
+        backgroundColor: color,
+        width: "4.5em",
+        height: "4.5em"
+    }
+}
